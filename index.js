de0e97e0cce0bf451e1103910fa4495d37c78b60
aringo <37981873+hakanarmo@users.noreply.github.com>
2019-02-04 00:49:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,155 +1,137 @@
-const Discord = require('discord.js')
-const client = new Discord.Client();
+const Discord = require('discord.js');
+const botconfig = require("./botconfig.json");
+const client = new Discord.Client(); 
 
-function clean(text) {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
+const prefix = '-' 
+const ownerID = '296048748877053953'; 
+const db = require('quick.db');
+
+const serverStats = {
+  guildID: '491773464076877825',
+  ticketCategoryID: '492083224869077032'
 }
 
-const prefix = "ti!";
-const token = " ";
-const embedColor = 0xE52B50;
-
-
-
-client.on("ready", () => {
-  client.user.setGame(prefix + `new`);
-});
-
-client.on("message", (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  if (message.content.toLowerCase().startsWith(prefix + `help`)) {
-    const embed = new Discord.RichEmbed()
-    .setTitle(`Ticket Bot Help`)
-    .setColor(embedColor)
-    .setDescription(`Hello! I'm Ticket Manager, the Discord bot for super cool support ticket stuff! Here are my commands:`)
-    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident\n[${prefix}add]() > Adds a member to a ticket\n[${prefix}remove]() > Removes a member from a ticket`)
-    message.channel.send({ embed: embed });
-  }
-
-
-  if (message.content.toLowerCase().startsWith(prefix + `new`)) {
-    const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "Support")) {
-    const embed0 = new Discord.RichEmbed()
-    .setColor(embedColor)
-    .addField(`Ticket Bot`, `This server doesn't have a \`Support\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`)
-    message.channel.send({ embed: embed0 });
-    return
+// Tickets
+client.on('message', async message => {
+if (message.author.bot) return;
+if (message.channel.type !== 'text') {
+    let active = await db.fetch(`support_${message.author.id}`);
+    let guild = client.guilds.get(serverStats.guildID);
+    let channel, found = true;
+    try {
+        if (active) client.channels.get(active.channelID)
+            .guild;
+    } catch (e) {
+        found = false;
     }
-    if (message.guild.channels.exists("name", "ticket-" + message.author.username)) {
-    const embed1 = new Discord.RichEmbed()
-    .setColor(embedColor)
-    .addField(`Ticket Bot`, `You already have a ticket open.`)
-    message.channel.send({ embed: embed1 });
-    return
-    }
-    message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-        let role = message.guild.roles.find("name", "Support");
-        let role2 = message.guild.roles.find("name", "@everyone");
-        c.overwritePermissions(role, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        c.overwritePermissions(role2, {
-            SEND_MESSAGES: false,
-            READ_MESSAGES: false
-        });
-        c.overwritePermissions(message.author, {
-            SEND_MESSAGES: true,
-            READ_MESSAGES: true
-        });
-        const embed2 = new Discord.RichEmbed()
-        .setColor(embedColor)
-        .addField(`Ticket Bot`, `Your ticket has been created in ` + c.toString())
-        .setTimestamp();
-        message.channel.send({ embed: embed2 });
+    if (!active || !found) {
+        active = {};
+        channel = await guild.createChannel(`${message.author.username}-${message.author.discriminator}`)
+            channel.setParent(serverStats.ticketCategoryID)
+            channel.setTopic(`-close to close the ticket | Support for ${message.author.tag} | ID: ${message.author.id}`)
 
-        const embed3 = new Discord.RichEmbed()
-        .setColor(embedColor)
-        .addField(`Hey ${message.author.username}!`, `Our **Support Team** will be with you shortly. Please explain your reason for opening the ticket in as much detail as possible.`)
-        .setTimestamp();
-        c.send({ embed: embed3 });
-    }).catch(console.error);
-  }
-
-  if (message.content.toLowerCase().startsWith(prefix + `add`)) {
-    if (!message.channel.name.startsWith(`ticket-`)) {
-    const embed4 = new Discord.RichEmbed()
-    .setColor(embedColor)
-    .addField(`Ticket Bot`, `You can't use the this outside of a ticket channel.`)
-    message.channel.send({ embed: embed4 });
-    return
+              channel.overwritePermissions("491776930190852106", { 
+                  VIEW_CHANNEL: false,
+                  SEND_MESSAGES: false,
+                  ADD_REACTIONS: false
+                });
+              
+        let author = message.author;
+        const newChannel = new Discord.RichEmbed()
+            .setColor(botconfig.red)
+            .setAuthor(author.tag, author.avatarURL)
+            .setFooter('Support Ticket Created!')
+            .addField('User', author)
+            .addField('ID', author.id)
+        await channel.send(newChannel);
+        const newTicket = new Discord.RichEmbed()
+            .setColor(botconfig.blue)
+            .setAuthor(`Hello, ${author.username}`, author.avatarURL)
+            .setFooter('Support Ticket Created!')
+        await author.send(newTicket);
+        active.channelID = channel.id;
+        active.targetID = author.id;
     }
-    addedmember = message.mentions.members.first();
-    message.channel.overwritePermissions(addedmember, { SEND_MESSAGES : true, VIEW_CHANNEL : true});
+    channel = client.channels.get(active.channelID);
+    const dm = new Discord.RichEmbed()
+        .setColor(botconfig.blue)
+        .setAuthor(`Thank you, ${message.author.username}`, message.author.avatarURL)
+        .setFooter(`Your message has been sent - A staff member will be in contact soon.`)
+    await message.author.send(dm);
+    if (message.content.startsWith(prefix + 'close')) return;
     const embed5 = new Discord.RichEmbed()
-    .setColor(embedColor)
-    .addField(`Ticket Bot`, '**' + addedmember + `** has been added to the ticket. Remove with [${prefix}remove]().`)
-    message.channel.send({ embed: embed5 });
-
-  }
-
-  if (message.content.toLowerCase().startsWith(prefix + `remove`)) {
-    if (!message.channel.name.startsWith(`ticket-`)) {
-    const embed6 = new Discord.RichEmbed()
-    .setColor(embedColor)
-    .addField(`Ticket Bot`, `You can't use the this outside of a ticket channel.`)
-    message.channel.send({ embed: embed6 });
-    return
+        .setColor(botconfig.red)
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setDescription(message.content)
+        .setFooter(`Message Received - ${message.author.tag}`)
+    await channel.send(embed5);
+    db.set(`support_${message.author.id}`, active);
+    db.set(`supportChannel_${channel.id}`, message.author.id);
+    return;
+}
+let support = await db.fetch(`supportChannel_${message.channel.id}`);
+if (support) {
+    support = await db.fetch(`support_${support}`);
+    let supportUser = client.users.get(support.targetID);
+    if (!supportUser) return message.channel.delete();
+    if (message.content.toLowerCase() === '-close') {
+        const complete = new Discord.RichEmbed()
+            .setColor(botconfig.blue)
+            .setAuthor(`Hey, ${supportUser.tag}`, supportUser.avatarURL)
+            .setFooter('Ticket Closed -- Coding Budgies')
+            .setDescription('*Your ticket has been marked as complete. If you wish to reopen it, or create a new one, please send a message to the bot.*')
+        supportUser.send(complete);
+        message.channel.delete();
+        db.delete(`support_${support.targetID}`);
+        let inEmbed = new Discord.RichEmbed()
+            .setTitle('Ticket Closed!')
+            .addField('Support User', `${supportUser.tag}`)
+            .addField('Closer', message.author.tag)
+            .setColor(botconfig.red)
+        const staffChannel = client.channels.get('491796908084559883'); //Create a log channel and put id here
+        staffChannel.send(inEmbed);
     }
-    removedmember = message.mentions.members.first();
-    message.channel.overwritePermissions(removedmember, { SEND_MESSAGES : false, VIEW_CHANNEL : false});
-    const embed7 = new Discord.RichEmbed()
-    .setColor(embedColor)
-    .addField(`Ticket Bot`, '**' + removedmember + '** has been removed from the ticket.')
-    message.channel.send({ embed: embed7 });
-  }
-
-  if (message.content.toLowerCase().startsWith(prefix + `close`)) {
-    if (!message.channel.name.startsWith(`ticket-`)) {
-    const embed8 = new Discord.RichEmbed()
-    .setColor(embedColor)
-    .addField(`Ticket Bot`, `You can't use the this outside of a ticket channel.`)
-    message.channel.send({ embed: embed8 });
-    return
-    }   
-
-    const embed9 = new Discord.RichEmbed()
-    .setColor(embedColor)
-    .addField(`Ticket Bot`, 'Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.')
-    message.channel.send({ embed: embed9 })
-    .then((m) => {
-      message.channel.awaitMessages(response => response.content === '-confirm', {
-        max: 1,
-        time: 10000,
-        errors: ['time'],
-      })
-      .then((collected) => {
-          message.channel.delete();
-        })
-        .catch(() => {
-          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-              m2.delete();
-          }, 3000);
-        });
+    const embed4 = new Discord.RichEmbed()
+        .setColor(botconfig.red)
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setFooter(`Message Received - Coding Budgies`)
+        .setDescription(message.content)
+    client.users.get(support.targetID)
+        .send(embed4);
+    message.delete({
+        timeout: 10000
     });
-  }
-
+    embed4.setFooter(`Message Sent -- ${supportUser.tag}`)
+        .setDescription(message.content);
+    return message.channel.send(embed4);
+}
 });
 
-function response(c) {
-  while (true) {
-    client.on("message", (message) => {
-      if(message.channel == c) {
-        return message.content;
-      }
-    });
-  }
-};
+// Command Handler
+client.on('message', message => {
 
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLowerCase();
 
-client.login(process.env.BOT_TOKEN)
+    if (message.author.bot) return; 
+    if (!message.content.startsWith(prefix)) return; 
+
+    try {
+
+        delete require.cache[require.resolve(`./commands/${cmd}.js`)];
+
+        let commandFile = require(`./commands/${cmd}.js`); 
+        commandFile.run(client, message, args);
+ 
+    } catch (e) { 
+        console.log(e.stack);
+    }
+});
+
+client.on('ready', async () => {
+    client.user.setActivity(`-help| ${client.guilds.size} Servers`, {type: "PLAYING"});
+})
+
+client.on('ready', () => console.log('Launched!'));
+
+client.login('NDkyMTY5MDU0OTIxMDMxNjgw.DoSf2g.WSG-fFQDeioouj6Oegpni7z_vZo');
