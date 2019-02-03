429ccf39daeafcdd7ed77fbf7d35a2fc385efa9c
aringo <37981873+hakanarmo@users.noreply.github.com>
2019-02-04 00:12:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,211 +1,162 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const util = require('util');
+const Discord = require('discord.js');
+const fs = require('fs');
 
 const Enmap = require('enmap');
 
-
-//Create Bot
+//CreateBot
 const bot = new Discord.Client();
 const TicketSystem = new Enmap();
-//Bot Stuff
-bot.commands = new Discord.Collection();
-var ServerName = "OneNationGaming Schedule Bot ";
 
-//Make bot login on discord.
-bot.login(process.env.BOT_TOKEN)
+bot.login(process.env.BOT_TOKEN);
 
-// When Bot has logged in do this
+// Important
+let ServerID = '541286273223098368'; // Server ID ( Right click your server Icon > Copy ID > Place here.) IMPORTANT.
+let ServerName = 'PUBG X Mates ★'; // Server name 
+let Activity = 'Hey you!'; // What the bot is playing
+let PREFIX = '$'; // Change me if you wish too
+
+let AdminRoleName = 'Moderators'; // Change me to the Admin role of your server ( EXACT SAME NAME )
+let EveryoneRoleName = '@everyone'; // Dont Change
+
+let SupportIcon = 'https://marketplace.magento.com/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/i/c/icon-medium-transparent.png'; // Icon Next to support tickets
+let SupportChannelCatagoryID = '541742094629339157'; // Support Ticket Catagory ( Create a new catagory > Right Click > CopyID > Paste here) IMPORTANT.
+let EmbedColor = 0x24ceff; // Embed Color 
+
+// When bot has logged into discord do this.
 bot.on("ready", async() =>{
-    bot.user.setActivity("Message to Schedule Training")
+    console.log(`\n${bot.user.username} is now online!\nConnected to : ${bot.guilds.size} discord(s) servers.`)
+    bot.guilds.forEach(guild =>{
 
-  const LogChannelID = "541291124669677602"
+        console.log(`Server Name : ${guild.name} | Server ID : ${guild.id} | Member Count : ${guild.memberCount} | Created : ${guild.createdAt}`);
+    })
 
-  var currentDate = new Date().toLocaleDateString();
-  var currentTime = new Date().toLocaleTimeString();
-
-  var embedLog = new Discord.RichEmbed()
-    .setTitle(`${ServerName} Log`)
-    .setDescription(`**${bot.user.username}** : **Has Started**\n\n${bot.user.username} is now **online**!\n\nConnteted to : **${bot.guilds.size}** discord(s) servers.`)
-    .setFooter(`Date: ${currentDate} Time: ${currentTime}`)
-    .setColor(0x800080)
-
-  var readyChannel = bot.channels.get(LogChannelID);
-  readyChannel.send(embedLog).then(console.log(`Send Ready!`)).catch(console.error)
-
-  //Console log the bot is online an ready for commands & also show how many servers the bot is connected to
-  console.log(`\n${bot.user.username} is now online!\nConnected to : ${bot.guilds.size} discord(s) servers\nReady for commands!`);
+    bot.user.setActivity(Activity);
+    
 });
 
-//Check if message was sent
-bot.on("message", async message =>{
-  let prefix = "ong!";
-  //If the message is from the bot. Dont do shit
-  if(message.author.bot) return;
+// Manage Messages.
+bot.on("message", async message => {
+    let prefix = PREFIX;
+    if(message.author.bot) return;
+
+    Support(message, prefix)
+
+    if (!message.content.startsWith(prefix)) return
+
+});
+
+async function Support(message, prefix){
+
+    // check if the message sent is inside dms 
+    if (message.channel.type == 'dm')
+    {
+        ToConsole(`Support Ticket: ${message.author.username} has sent a support ticket.`)
+
+        let active = await TicketSystem.get(`support_${message.author.id}`)
+
+        let guild = bot.guilds.get(ServerID);
+        let channel, found = true;
+        let author = message.author;
+
+        // Check if they have already created a support ticket,
+        try{
+            if(active) bot.channels.get(active.channelID).guild;
+        } catch(e){
+            found = false;
+        }
+
+        // if no support ticket was found then we need to create one.
+        if(!active || !found) {
+            ToConsole(`Support Ticket: No Channel has been found, creating a new support channel.`);
+            active = {};
+
+            let admin = guild.roles.find('name', AdminRoleName);
+            let everyone = guild.roles.find('name', EveryoneRoleName);
+
+            // Create a new channel
+            channel = await guild.createChannel(`${message.author.username} - ID ${author.discriminator}`);
+            // Channel permissions and other stuff.
+            await channel.setParent(SupportChannelCatagoryID); // Sets the channel inside the correct catagory.
+            await channel.setTopic(`${prefix}complete to complete this ticket | ${message.author.username} | Support ID: ${message.author.id}`); // Changes the channel we have just created topic.
+            
+            // Overwrites the current permissions ( to avoid everyone from being able to see the channels apart from admins of course. )
+            await channel.overwritePermissions(admin, {'READ_MESSAGES': true, 'SEND_MESSAGES': true,'MANAGE_MESSAGES': false,})
+            await channel.overwritePermissions(everyone, {'READ_MESSAGES': false, 'SEND_MESSAGES': false,'MANAGE_MESSAGES': false,})
 
 
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+            // Send embed to the new created support ticket channel.
+            const SupportEmbed = CreateEmbed(`${everyone},\n${author.username} has created a support ticket.`, `${ServerName} Support Ticket`, `Reply to respond to the support ticket.`);
+            const ReplyToTicketCreator = CreateEmbed(`Thank you for your Support Ticket. A **${ServerName}** staff member will be with you shortly.`, `${ServerName} Support Ticket`, `Staff member will be intouch shortly.`);
 
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+            // Send embeds
+            await channel.send(SupportEmbed);
+            await author.send(ReplyToTicketCreator);
 
-  if(commandfile) commandfile.run(bot, message, args);
+            active.channelID = channel.id;
+            active.targetID = author.id;
 
-  //TICKET SYSTEM
-  //Player Direct Message Ticket
-  if (message.channel.type == 'dm'){
-    console.log("Ticket Sent")
+            // Console log what has just happened.
+            ToConsole(`Support Ticket: Created, Active Channel ID : ${active.channelID}, Active author ID : ${active.targetID}`);
+        }
 
-    //Get Activity info
-    let active = await TicketSystem.get(`support_${message.author.id}`);
+        channel = bot.channels.get(active.channelID);
 
-    //Get the GUILD ID
-    let guild = bot.guilds.get('541286273223098368'); // CHANGE ME DEPENDING ON THE SERVER
-      
-    //variables
-    let channel, found = true;
+        // Send what the member sent to the bot to the Support ticket channel.
+        const supportMessage = CreateEmbed(message.content, `${message.author.username}`, `Message Recived -- ${message.author.tag}`);
+        await channel.send(supportMessage);
+        ToConsole(`${message.author.username} said : ${message.content}`);
 
-    // Active check if the support channel still exists
-    try {
-      if(active) bot.channels.get(active.channelID).guild;
-    } catch (e) {
-      found = false;
+        //Update User Data
+        TicketSystem.set(`support_${message.author.id}`, active);
+        TicketSystem.set(`supportChannel_${channel.id}`, message.author.id);
+        
+        return;
     }
 
-    if(!active || !found ){
+    // Staff sending messages back.
+    let support = await TicketSystem.get(`supportChannel_${message.channel.id}`);
 
-      console.log(`No Active Found (LN: 116) Creating active`);
+    if(support){
+        support = await TicketSystem.get(`support_${support}`);
 
-      active = {};
+        // Make sure the user we are talking to is still inside the discord ( Hasnt left the server etc)
+        let SupportUser = bot.users.get(support.targetID);
+        if(!SupportUser) return message.channel.delete(); // User no longer exists inside the discord server so we will delete the support channel.
 
-      let theTeam = guild.roles.find('name', 'TheTeam');
-      let FieldTrainingOfficer = guild.roles.find('name', 'Field Training Officer');
-      let Everyone = guild.roles.find("name", "@everyone");
-      //Create Channel Stuff
-      channel = await guild.createChannel(`${message.author.username} - ${message.author.discriminator}`);
+        // If a staff member types PREFIXcomplete it will complete the support ticket and close it.
+        if(message.content.toLowerCase() === `${prefix}complete`){
+            const completeTicket = CreateEmbed(`Hey ${SupportUser.username},\nSupport Ticket has been marked as **complete** by ${ServerName} staff Member: **${message.author.username}**.\n\nIf you wish to reopen or create a new ticket reply back to me!\nHave a wonderful day ${SupportUser.username}`, `Ticket Complete`, `Ticket Closed -- ${ServerName}`);
+            SupportUser.send(completeTicket);
+            message.channel.delete();
+            ToConsole(`${message.author.username} completed ${SupportUser.username} support ticket.`);
+            return TicketSystem.delete(`support_${support.targetID}`);
+        }
 
-      await channel.setParent('541738461640654868'); // CHANGE ME DEPENDING ON THE SERVER
-      console.log(channel.parentID);
-      await channel.setTopic(`ong!complete to close the ticket | Support for ${message.author.tag} | ID: ${message.author.id}`);
-      await channel.overwritePermissions(theTeam, {'READ_MESSAGES': true, 'SEND_MESSAGES': false,'MANAGE_MESSAGES': false,})
-      await channel.overwritePermissions(FieldTrainingOfficer, {'READ_MESSAGES': true, 'SEND_MESSAGES': true,'MANAGE_MESSAGES': false,})
-      await channel.overwritePermissions(Everyone, {'READ_MESSAGES': false, 'SEND_MESSAGES': false,'MANAGE_MESSAGES': false,})
+        const SendSupportTicketMessage = CreateEmbed(message.content, `${ServerName} Support Reply`, `Message Recived -- ${ServerName} Staff Team`);
+        bot.users.get(support.targetID).send(SendSupportTicketMessage);
+        ToConsole(`Staff Member (${message.author.username}) said : ${message.content}`);
+        message.delete({timeout: 1000})
 
+        SendSupportTicketMessage.setFooter(`Support Ticket Reply -- Staff Member: ${message.author.username}`);
+        return message.channel.send(SendSupportTicketMessage);
 
-      //Create a user object to make everything easier.
-      let author = message.author;
-      //Create a Embed for the new channel being created
-      const newChannel = new Discord.RichEmbed()
-        .setColor(0x800080)
-        .setDescription(`ID : ${author.id}`)
-        .setAuthor(`${author.tag} Training Request`)
-        .setFooter(`Reply here to respond to the request.`)
-
-      //Send Message to the new created channel
-      await channel.send(newChannel);
-
-      // Create another embed to send to the client, telling them they have created a ticket.
-      const newTicket = new Discord.RichEmbed()
-        .setColor(0x800080)
-        .setAuthor(`${author.tag}️`)
-        .setFooter(`Training Request`)
-
-      //Send Message to the new created channel
-      await author.send(newTicket);
-
-
-      //Update Data.
-      active.channelID = channel.id;
-      active.targetID = author.id;
-
-      console.log(`Training Request - Active ChannelID : ${active.channelID}\nTraining Request - Active TargetID : ${active.targetID}`);
     }
 
+}
 
-    channel = bot.channels.get(active.channelID);
+function CreateEmbed(d, a, f)
+{
+    const Embed = new Discord.RichEmbed()
+    .setColor(EmbedColor)
+    .setDescription(d)
+    .setAuthor(a, SupportIcon)
+    .setFooter(f)
+    .setTimestamp();
 
-    //create the DM embed
-    const dm = new Discord.RichEmbed()
-      .setColor(0x800080)
-      .setAuthor(`Thank you, ${message.author.tag}`)
-      .setFooter(`Your request has been submitted -- A Field training Officer will be in contact soon`)
+    return Embed;
+}
 
-    //Send Message to the new created channel
-      await message.author.send(dm);
-
-    //Create ticket embed what staff see
-    const embed = new Discord.RichEmbed()
-      .setColor(0x800080)
-      .setAuthor(`${message.author.tag}`)
-      .setDescription(message.content)
-      .setFooter(`Message Recieved -- ${message.author.tag}`)
-
-    //Send Embed
-      await channel.send(embed);
-
-    //Update Data & return
-    TicketSystem.set(`support_${message.author.id}`, active);
-    TicketSystem.set(`supportChannel_${channel.id}`, message.author.id);
-    return;
-  }
-
-
-  //STAFF SIDE OF TICKET SYSTEM
-  //Fetch Support stuffs
-
-  let support = await TicketSystem.get(`supportChannel_${message.channel.id}`);
-
-  //if support is defined (Not null ) that means the channel the message was sent in is a support channel!
-  if(support){
-    //update the support object to the one we set before.
-    support = await TicketSystem.get(`support_${support}`);
-
-    //Check if the user is still in the server (guild hasnt left)
-    let supportUser = bot.users.get(support.targetID);
-    if(!supportUser) return message.channel.delete(); // If user doesnt exist no more delete the support ticket. Clears room for more.
-
-    //ong!complete command here
-    if(message.content.toLowerCase() === 'ong!complete'){
-      //create embed
-      const complete = new Discord.RichEmbed()
-        .setColor(0x800080)
-        .setAuthor(`Hey, ${supportUser.tag}`)
-        .setFooter(`Training Request Closed -- OneNationGaming`)
-        .setDescription(`Your Training Request has been marked as **complete**. If you wish to reopen this, or create a new Training Request reply back to me! `)
-
-      //Send Embed
-      supportUser.send(complete)
-
-      // Delete the support channel as its no longer needed
-      message.channel.delete();
-
-      //Return & delete the guild object .
-      return TicketSystem.delete(`support_${support.targetID}`);
-    }
-
-    //create embed
-    const embed = new Discord.RichEmbed()
-      .setColor(0x800080)
-      .setAuthor(`OneNationGaming Training Request`)
-      .setDescription(message.content)
-      .setFooter(`Training Request -- OneNationGaming Team`)
-
-    //Send Embed
-    bot.users.get(support.targetID).send(embed)
-
-    message.delete({timeout: 1000})
-
-    //Modify the embed sent in the ticket channel to show when the message was sent to the user.
-    embed.setFooter(`Training Request Reply -- TeamMember: ${message.author.tag}`)
-
-    //Return the same bot message into support chat.
-    return message.channel.send(embed)
-
-  }
-
-  if (!message.content.startsWith(prefix)) return
-
-
-
-})
+function ToConsole(m)
+{
+    console.log(`\n${ServerName} Console Log\n${m}\n`);
+}
